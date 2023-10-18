20df977e38f03ed1992377f6819d5610efe67312
Parsa Estakhri <parsaestakhry03@gmail.com>
2023-10-18 12:11:09 +0330

    made changes to items

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -10,4 +10,12 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        
+        #strip all the whitespaces from strings
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_names != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name] = value.strip()
         return item
